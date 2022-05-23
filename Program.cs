e25ffa2a5bc775de34f2b8eb766c0f51a7ac57a5
g-house <gabrielluiz-back@hotmail.com>
2022-05-22 22:10:08 -0300

    MS_Learn_001

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-Random random = new Random();
+﻿Random random = new Random();
 int daysUntilExpiration = random.Next(12);  
 int discountPercentage = 0;
 if(daysUntilExpiration <= 10){
     if( (daysUntilExpiration <= 5) && (daysUntilExpiration >1)){
         Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!");
+        discountPercentage = 10;
     }else if(daysUntilExpiration == 1){
         Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
         discountPercentage =20;
--- README.md
+++
@@ -1,3 +0,0 @@
-# Microsoft_Learn
-    * Atividades do Microsoft Learn
-    * Blocos de código.
